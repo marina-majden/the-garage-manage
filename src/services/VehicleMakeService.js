92dba6005baa0ee149c456d1a879326f7cb53103
Marina Majdenić <marina.majden@gmail.com>
2025-02-25 20:11:11 +0100

    build: add service classes for vehicle makes and models

---
+++ src/services/VehicleMakeService.js
@@ -0,0 +1,8 @@
+import { BaseService } from './BaseService';
+
+export class VehicleMakeService extends BaseService {
+  constructor() {
+    super('vehicleMakes');
+  }
+  
+}
---
+++ src/services/VehicleModelService.js
@@ -0,0 +1,17 @@
+import { BaseService } from './BaseService';
+import { doc, getDoc } from "firebase/firestore";
+
+export class VehicleModelService extends BaseService {
+  constructor() {
+    super('vehicleModels');
+  }
+  async updateModel(id, data) {
+  await updateDoc(doc(this.collection, id), data);
+}
+async getById(id) {
+  const docRef = doc(this.collection, id);
+  const docSnap = await getDoc(docRef);
+  return docSnap.exists() ? { id: docSnap.id, ...docSnap.data() } : null;
+}
+
+}
