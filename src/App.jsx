088ddcc08f96456a2fd6d7562c6719823bbfd21f
Marina Majdenić <marina.majden@gmail.com>
2025-02-26 17:27:53 +0100

    style: change colors on body

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,17 @@
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import { ThemeProvider } from '@mui/material/styles';
+import theme from './utils/theme';
 import Homepage from './pages/Homepage';
 
 
 function App() {
   return (
     <BrowserRouter>
+    <ThemeProvider theme={theme}>
       <Routes>
-        <Route path="/" element={<Homepage />} />
+          <Route path="/" element={<Homepage />} />
    
-      </Routes>
+      </Routes></ThemeProvider>
     </BrowserRouter>
   );
 }
